1f157747c37d1e93a9334409529f0f0ac885e0ef
MSI KATANA <mohammed.mahmood@semaai.com>
2023-02-04 23:54:57 +0530

    Note added for conflated channel

--- src/main/kotlin/channel/Note.kt
+++ src/main/kotlin/channel/Note.kt
@@ -5,3 +5,14 @@ package channel
  * by sending and receiving messages between different coroutines.
  * These are basically pipelines
  */
+
+
+/**
+ * There is a third type of buffered channel [ConflatedChannel],
+ * based on the idea that it's fine if elements that
+ * were emitted are lost. This means that this type of
+ * channel has a buffer of only one element,
+ * and whenever a new element is sent,
+ * the previous one will be lost.
+ * This also means that the sender will never be suspended.
+ */
