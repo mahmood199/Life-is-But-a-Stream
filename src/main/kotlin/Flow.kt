3a96e0151613d2bd2186e03624268cb357808117
MSI KATANA <mohammed.mahmood@semaai.com>
2023-02-06 23:50:26 +0530

    Flow example added

---
+++ src/main/kotlin/Flow.kt
@@ -0,0 +1,19 @@
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+import kotlin.random.Random
+
+
+fun main() = runBlocking {
+    generateRandomNumbers(10).collect {
+        println(it)
+    }
+    println("That's all folks!")
+}
+
+
+fun generateRandomNumbers(count: Int) = flow {
+    for (i in 0 until count) {
+        delay(1000)
+        emit(Random.nextInt(1, 100))
+    }
+}.flowOn(Dispatchers.IO)
