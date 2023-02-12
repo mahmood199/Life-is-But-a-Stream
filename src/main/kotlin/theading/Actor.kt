da2acba1594520ac619d9b7f58f5795ee7a5450e
Mohammed Mahmood Ahmed <gaudentas199@gmail.com>
2023-02-12 22:55:17 +0530

    Actors (#6)
    * actor added
    
    * actor added
    
    ---------
    
    Co-authored-by: MSI KATANA <mohammed.mahmood@semaai.com>

---
+++ src/main/kotlin/theading/Actor.kt
@@ -0,0 +1,59 @@
+package theading
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.channels.actor
+
+sealed class Action {
+    object INCREASE : Action()
+    object DECREASE : Action()
+}
+
+
+class Actor {
+
+    private var counter = 0
+    private val context = newSingleThreadContext("counterActor")
+    fun getCounter() = counter
+    lateinit var actorCounter : SendChannel<Action>
+    var st = ArrayDeque<Pair<Int, Int>>()
+    var smallestValue = Int.MIN_VALUE
+
+
+
+    private fun CoroutineScope.initActor() = actor<Action>(capacity = 20) {
+            for (msg in channel) {
+                when (msg) {
+                    Action.INCREASE -> counter++
+                    Action.DECREASE -> counter--
+                }
+            }
+        }
+
+
+    private suspend fun asyncIncrement(by: Int) = CoroutineScope(context).async {
+        for (i in 0 until by) {
+            actorCounter.send(Action.INCREASE)
+        }
+    }
+
+    private suspend fun asyncDecrement(by: Int)  =  CoroutineScope(context).async {
+        for (i in 0 until by) {
+            actorCounter.send(Action.DECREASE)
+        }
+    }
+
+    fun main(args: Array<String>) = runBlocking {
+        initActor().send(Action.DECREASE)
+        val workerA = asyncIncrement(2000)
+        val workerB = asyncIncrement(100)
+        val workerC = asyncDecrement(1000)
+        workerA.await()
+
+        workerB.await()
+        workerC.await()
+        print("counter [${getCounter()}]")
+    }
+
+}
+
