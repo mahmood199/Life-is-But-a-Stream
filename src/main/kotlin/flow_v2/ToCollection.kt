8d53308e000482e332c9283624bc399f84b1e03e
MSI KATANA <mohammed.mahmood@semaai.com>
2023-04-15 09:13:31 +0530

    Note added

--- src/main/kotlin/flow_v2/ToCollection.kt
+++ src/main/kotlin/flow_v2/ToCollection.kt
@@ -25,3 +25,11 @@ fun toCollectionRandomPercentages(count: Int, delayMs: Long) = flow {
         emit(Random.nextInt(1, 100))
     }
 }
+
+/**
+ *      These functions handle all bounded flows: zero items,
+ *      one item, or several items. However, they only work
+ *      for flows that will close themselves based on some
+ *      internal criterion. These functions will not work
+ *      well for flows that might emit objects indefinitely.
+ */
