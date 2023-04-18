f75b8c50a89ea8c0a5a01dd7e4dae846eee619e0
MSI KATANA <gaudentas199@gmail.com>
2023-04-19 01:17:52 +0530

    Sample Actor added
    Signed-off-by: MSI KATANA <gaudentas199@gmail.com>

--- src/main/kotlin/channel/Note.kt
+++ src/main/kotlin/channel/Note.kt
@@ -1,18 +1,23 @@
 package channel
 
 /**
- * [Channels] allow concurrent code to communicate with each other
- * by sending and receiving messages between different coroutines.
- * These are basically pipelines
+ *      [Channels] allow concurrent code to communicate with each other
+ *      by sending and receiving messages between different coroutines.
+ *      These are basically pipelines
+ */
+/**
+ *      Channel() looks like a constructor… but Channel is an interface,
+ *      not a class. So, Channel() is actually a top-level factory
+ *      function that creates Channel objects of a particular type,
+ *      based on its parameter.
  */
 
-
 /**
- * There is a third type of buffered channel [ConflatedChannel],
- * based on the idea that it's fine if elements that
- * were emitted are lost. This means that this type of
- * channel has a buffer of only one element,
- * and whenever a new element is sent,
- * the previous one will be lost.
- * This also means that the sender will never be suspended.
+ *      There is a third type of buffered channel [ConflatedChannel],
+ *      based on the idea that it's fine if elements that
+ *      were emitted are lost. This means that this type of
+ *      channel has a buffer of only one element,
+ *      and whenever a new element is sent,
+ *      the previous one will be lost.
+ *      This also means that the sender will never be suspended.
  */
